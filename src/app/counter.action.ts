bf0f91565807b296ced0d3147b7d12c202e6bbb0
smykytenk0 <mykytenk00s@gmail.com>
2021-04-01 20:24:29 +0300

    first commit, completed problem with counter and its increment, decrement and reset

---
+++ src/app/counter.action.ts
@@ -0,0 +1,5 @@
+import {createAction} from "@ngrx/store";
+
+export const increment = createAction('[Count Component] Increment');
+export const decrement = createAction('[Count Component] Decrement');
+export const reset = createAction('[Count Component] reset');
