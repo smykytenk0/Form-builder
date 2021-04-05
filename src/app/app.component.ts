3f24ebcad0c1003869593629273593b50ffb7ec6
smykytenk0 <mykytenk00s@gmail.com>
2021-04-05 20:38:33 +0300

    Refactored all project. Added two components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 
+export interface User {
+  email: string
+  password: string
+  id?: any
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title:"formbuilder"
+  users: User[]=[
+    {email:"123poin@gmail.com", password:"12345678", id: 1},
+    {email:"456point@gmail.com", password:"12312312", id:2}
+  ];
+
+  updateUsers(user: User) {
+    this.users.unshift(user)
+  }
 }
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
-import { StoreModule } from '@ngrx/store';
-import { reducers, metaReducers } from './store';
-import { LoginPageComponent } from './components/login-page/login-page.component';
-import { EffectsModule } from '@ngrx/effects';
-import { LoginPageEffects } from './store/effects/login-page.effects';
+import {AppComponent} from "./app.component";
+import {FormsModule} from "@angular/forms";
+import { LoginFormComponent } from './login-form/login-form.component';
+import { UsersComponent } from './users/users.component';
+
+
 
 @NgModule({
-  declarations: [AppComponent, LoginPageComponent],
+  declarations: [AppComponent, LoginFormComponent, UsersComponent],
   imports: [
     BrowserModule,
-    !environment.production ? StoreDevtoolsModule.instrument() : [],
-    StoreDevtoolsModule.instrument({maxAge: 25, logOnly: environment.production}),
-    StoreModule.forRoot(reducers, { metaReducers }),
-    EffectsModule.forRoot([LoginPageEffects]),  ],
+    FormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
