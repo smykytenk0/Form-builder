4ef50827be9086e9f7fe39842b4621feb23ba909
smykytenk0 <mykytenk00s@gmail.com>
2021-04-06 20:43:21 +0300

    Refactored. Added login logic.

--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,5 +1,5 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-import {User} from "../app.component";
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {AppComponent, User} from "../app.component";
 
 @Component({
   selector: 'app-login-form',
@@ -10,25 +10,58 @@ export class LoginFormComponent implements OnInit {
 
   @Output() onAdd: EventEmitter<User> = new EventEmitter<User>();
 
-  email='';
-  password='';
 
-  constructor() { }
+  email = '';
+  password = '';
+  isUserLogged: boolean;
+  users: User[]=[
+    {email:"123poin@gmail.com", password:"12345678", id: 1},
+    {email:"456point@gmail.com", password:"12312312", id:2}
+  ];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
-  onAddUser(){
-    if(this.email.trim() && this.password.trim()){
+  onAddUser() {
+    console.log(this.users);
+    if (this.email.trim() && this.password.trim()) {
       const user: User = {
         email: this.email,
-        password: this.password
+        password: this.password,
       };
 
       this.onAdd.emit(user);
 
-      this.password = this.email ="";
+      this.password = this.email = "";
     }
+  }
+
+  login() {
+    console.log(this.users);
+    if (this.email.trim() && this.password.trim()) {
+      const user: User = {
+        email: this.email,
+        password: this.password,
+      };
+      for(let i = 0; i<10; i++) {
+        if (this.users[i].email == this.email && this.users[i].password == this.password) {
+          this.isUserLogged = true;
+          break;
+        }
+        else {
+          this.email = this.password = "";
+          continue;
+        }
+      }
+      this.email = this.password = "";
+    }
+  }
+
+  logout(){
+    this.isUserLogged = !this.isUserLogged;
   }
 
 }
