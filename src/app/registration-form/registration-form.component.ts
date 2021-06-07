a09992b96620e8f44559717b9645a0a389653d36
smykytenk0 <mykytenk00s@gmail.com>
2021-06-07 19:59:18 +0300

    commit on last review

--- src/app/registration-form/registration-form.component.ts
+++ src/app/registration-form/registration-form.component.ts
@@ -1,22 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Store } from '@ngrx/store';
-import { Router } from '@angular/router';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Store} from '@ngrx/store';
+import {Router} from '@angular/router';
 
-import { AuthService } from '../shared/services/auth.service';
-import { User } from '../shared/interfaces/interfaces';
-import { StylesActions } from '../store/styles.actions';
+import {AuthService} from '../shared/services/auth.service';
+import {User} from '../shared/interfaces/interfaces';
+import {StylesActions} from '../store/styles.actions';
+import {Subject} from "rxjs";
+import {takeUntil} from "rxjs/operators";
 
 @Component({
   selector: 'app-registration-form',
   templateUrl: './registration-form.component.html',
   styleUrls: ['./registration-form.component.scss']
 })
-export class RegistrationFormComponent implements OnInit {
+export class RegistrationFormComponent implements OnInit, OnDestroy {
   form: FormGroup;
   formData: User;
+  private unsubscribeAll: Subject<any> = new Subject<any>();
 
-  constructor(private auth: AuthService, private store: Store, private router: Router) { }
+  constructor(private auth: AuthService, private store: Store, private router: Router) {
+  }
 
   ngOnInit(): void {
     this.form = new FormGroup({
@@ -28,9 +32,14 @@ export class RegistrationFormComponent implements OnInit {
 
   onSubmit(): void {
     this.formData = this.form.value;
-    this.auth.register(this.formData).subscribe();
+    this.auth.register(this.formData).pipe(takeUntil(this.unsubscribeAll)).subscribe();
     this.store.dispatch(StylesActions.setAuthStatus({payload: true}));
-    this.router.navigate(['/forms'])
+    this.router.navigate(['/forms']);
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribeAll.next();
+    this.unsubscribeAll.complete();
   }
 
 }
